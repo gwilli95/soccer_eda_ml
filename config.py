b39ae0bca3deb656cc6234d27ec95404d25fff26
gwilli95 <grantrwilliams7@gmail.com>
2025-12-29 17:35:39 -0500

    Code cleanup, data ingestion improvement, project paths/structuring

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 import pathlib
 
-KAGGLE_DATABASE = "datasets/hugomathien/soccer"
-SOURCE_PATH = pathlib.Path(__file__).resolve().parent
+SOCCER_DATABASE = "hugomathien/soccer"
+SOURCE_PATH = pathlib.Path.cwd().resolve().parent
 DATA_PATH = SOURCE_PATH.joinpath("data")
+NOTEBOOKS_PATH = SOURCE_PATH.joinpath("notebooks")
+MODELS_PATH = SOURCE_PATH.joinpath("models")
--- utils.py
+++ utils.py
@@ -1,40 +1,21 @@
 import os
-
-import pandas as pd
-import pathlib
-from sklearn.compose import ColumnTransformer
-from sklearn.impute import SimpleImputer
-#from config import DATA_PATH
-import sqlite3
-
-
-
-
-import kagglehub
 import shutil
-import os
-import sqlite3
-import pandas as pd
+import kagglehub
+from config import DATA_PATH
 
-def ingest_soccer_data(path):
-    # 1. Download (No auth setup needed if you have kaggle.json)
+def ingest_kaggle_database(path):
+    #1. Retrieve the download path
     download_path = kagglehub.dataset_download(path)
     
-    # 2. Find the sqlite file in the kagglehub cache
-    files = os.listdir(download_path)
-    sqlite_file = next(f for f in files if f.endswith('.sqlite'))
+    #2. Access the database file name
+    downloaded_files = os.listdir(download_path) #Lists downloaded files
+    sqlite_file = next(f for f in downloaded_files if f.endswith(".sqlite")) #Gets the first .sqlite file
+    
+    #3. Create the full file path (.sqlite file alone insufficient for access)
     full_path = os.path.join(download_path, sqlite_file)
     
-    # 3. Move it to your local project folder for the team to use
-    local_data_dir = "./data"
-    os.makedirs(local_data_dir, exist_ok=True)
-    local_file_path = os.path.join(local_data_dir, "database.sqlite")
+    #4. Create data folder for the path to go in
+    os.makedirs(DATA_PATH, exist_ok = True)
     
-    shutil.copy(full_path, local_file_path)
-    print(f"Database ready at: {local_file_path}")
-    
-    # 4. Quick check
-    conn = sqlite3.connect(local_file_path)
-    df = pd.read_sql_query("SELECT name FROM sqlite_master WHERE type='table';", conn)
-    conn.close()
-    return df
+    #5. Move full file path into the folder
+    shutil.copy(full_path, DATA_PATH)
